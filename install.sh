da587b993f55adbffecc5b25a3afb5c441a1cfbd
9hsas <KrypZero@Proton.ME>
2026-08-07 22:58:44 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo pacman -Sy --noconfirm git
+
+git clone https://github.com/real9hsas/9hsas-configs
+
+cd 9hsas-configs
+cp acsii.txt
+
+cd config
+
+cp -a fastfetch $HOME/.config
+cp -a fish $HOME/.config
+cp -a kitty $HOME/.config
+cp hyfetch.json $HOME/.config
+cp acsii.txt
+
+sudo pacman -Sy --noconfirm fastfetch fish kitty hyfetch
+
+cd ..
+cd ..
+
+fish
